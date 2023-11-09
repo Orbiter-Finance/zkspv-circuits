3b2144deaeaa12b7a36d80c14bbb8ce3867d4b5e
ZeroKPunk <jusonxia@gmail.com>
2023-11-09 12:56:55 +0800

    update bench script

--- scripts/bench_arbitration.sh
+++ scripts/bench_arbitration.sh
@@ -1,12 +1,10 @@
-# rm data/arbitration/*.snark
-# rm data/arbitration/*.yul
-# rm data/arbitration/*.calldata
-# rm data/arbitration/*.pk
-# rm cache_data/arbitration/*.snark
-# rm cache_data/arbitration/*.pk
+rm data/arbitration/*.snark
+rm data/arbitration/*.yul
+rm data/arbitration/*.calldata
+rm data/arbitration/*.pk
+#rm cache_data/arbitration/*.snark
+#rm cache_data/arbitration/*.pk
 
 rm arbitration_business
 cargo clean
-cargo build --release --bin arbitration_business
-cp target/release/arbitration_business .
-./arbitration_business
+MEASURE=1 cargo run --release --bin arbitration_business
